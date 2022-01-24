483b18e12752d54621c73fcb40abf673a2698654
Devlin Macpherson <71355015+devlinmacpherson@users.noreply.github.com>
2022-01-24 16:39:31 -0500

    Update trainStopSwitch.ino

--- trainStopSwitch.ino
+++ trainStopSwitch.ino
@@ -1,5 +1,6 @@
 #include <analogWrite.h>
 
+
 int irVal1;
 int irVal2;
 int irVal3;
@@ -36,39 +37,52 @@ void setup() {
   pinMode(dacPin, OUTPUT);
 
   Serial.begin(115200);
+
 }
 
 
 void loop() {
   setTrainSpeed(trainSpeed);
+  setTrack(switchVal);
+ 
   if(Serial.available()){
     extractCommandValues();
   }
   readAndSendSensors();  
+  Serial.println(switchVal);
+ 
 }
 
 void setTrainSpeed(int trainSpeed) {
   dacWrite(dacPin, trainSpeed);
 }
 
+void setTrack(int switchVal){
+  digitalWrite(switchPin, switchVal);
+
+}
+
 void extractCommandValues() {
     String commandSequence = Serial.readStringUntil(';');
+    
     String switchString = getValueFromString(commandSequence, ',', 0);
     String speedString = getValueFromString(commandSequence, ',', 1);
     switchVal = switchString.toInt();
     trainSpeed = speedString.toInt();
+
+    Serial.println(switchVal);
 }
 
 void readAndSendSensors() {
-  Serial.print(analogRead(potPin));
-  Serial.print(',');
-  Serial.print(analogRead(irPin1));
-  Serial.print(',');
-  Serial.print(analogRead(irPin2));
-  Serial.print(',');
-  Serial.print(analogRead(irPin3));
-  Serial.print(',');
-  Serial.println(analogRead(irPin4));  
+//  Serial.print(analogRead(potPin));
+//  Serial.print(',');
+//  Serial.print(analogRead(irPin1));
+//  Serial.print(',');
+//  Serial.print(analogRead(irPin2));
+//  Serial.print(',');
+//  Serial.print(analogRead(irPin3));
+//  Serial.print(',');
+//  Serial.println(analogRead(irPin4));  
 }
 
 
